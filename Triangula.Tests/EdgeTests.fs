fcfef5c1f67a4c845b45a812f7bb87a04d5e9e9f
Ivan Glushchenko <ivan.glushchenko@outlook.com>
2014-11-15 21:03:08 -0500

    first commit

---
+++ Triangula.Tests/EdgeTests.fs
@@ -0,0 +1,21 @@
+﻿namespace Triangula.Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open Definitions
+
+
+[<TestClass>]
+type EdgeTests() = 
+
+    [<TestMethod>]
+    member x.TestEdgesIntersect () = 
+//    val e1 = Edge(Pos(1, 1), Pos(1, 2))
+//    val e2 = Edge(Pos(2, 1), Pos(2, 2))
+//    val e3 = Edge(Pos(1, 1), Pos(3, 2))
+//    assert(e1.intersect(e3) === false)
+//    assert(e2.intersect(e3) === true)
+        //Edge.
+        let testVal = 1
+        Assert.AreEqual(1, testVal)
